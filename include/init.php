fb2115c0b7b4b95a75e93520a8b8e6076c7a31ba
Karo800 <karolynn75@hotmail.com>
2025-02-11 16:57:36 +0100

    develop template + regitration

---
+++ include/init.php
@@ -0,0 +1,44 @@
+<?php
+// ------ CONNEXION BDD
+$connect_db = new PDO('mysql:host=localhost;dbname=shop', 'root', '', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+]);
+
+// echo '<pre>'; print_r($connect_db); echo '</pre>';
+
+
+// ------ SESSION
+session_start();
+
+
+// ------ CHEMIN
+// echo '<pre>'; print_r($SERVER); echo '</pre>';
+define('RACINE_SITE', $_SERVER['DOCUMENT_ROOT'] . '/');
+// echo '<pre>'; print_r($RACINE_SITE); echo '</pre>';
+// Cette constante retourne le chemin physique du dossier htdocs sur le serveur, de notre dossier 'shop' sur le serveur.
+// Lors de l'enregistrement d'images/photos , nous aurons besoin du chemin complet dossier images pour enregistrer la photo
+// echo RACINE_SITE . 'shop/assets/images/product.jpg';
+
+define("URL", "http://localhost/PHP%20gg/shop/");
+// <img src="URL
+// Cette constante servira à enregistrer l'URL d'une photo/image dans la BDD, on ne pas conserver la photo physiquement dand la BDD, donc on définit une URL vers le bon dossier.
+
+
+// ------------ VARIABLES
+$content = '';
+
+
+//------------ FAILLES XSS
+foreach ($_POST as $key => $value) {
+     $_POST[$key] = htmlentities(addslashes(trim($value)));
+}
+
+foreach ($_GET as $key => $value) {
+    $_GET[$key] = htmlentities(addslashes(trim($value)));
+}
+// trim() : fonction prédéfinie qui supprime les espaces en début et fin de chaînes de caractères.
+
+//------------ INCLUSIONS FONCTIONS
+require_once("functions.php");
+
--- inscription.php
+++ inscription.php
@@ -1,150 +1,81 @@
-<!DOCTYPE html>
-<html>
+<?php
+require_once('include/init.php');
 
-<head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-  <link rel="shortcut icon" href="assets/images-famma/favicon.png" type="" />
-  <title>Famms - Fashion HTML Template</title>
-  <!-- bootstrap core css -->
-  <link
-    rel="stylesheet"
-    type="text/css"
-    href="assets/css-famma/bootstrap.css" />
-  <!-- font awesome style -->
-  <link href="assets/css-famma/font-awesome.min.css" rel="stylesheet" />
-  <!-- Custom styles for this template -->
-  <link href="assets/css-famma/style.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="assets/css-famma/responsive.css" rel="stylesheet" />
-</head>
 
-<body class="sub_page">
-  <div class="hero_area">
-    <!-- header section strats -->
-    <header class="header_section">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg custom_nav-container">
-          <a class="navbar-brand" href="index.php"><img width="250" src="assets/images-famma/logo.png" alt="#" /></a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class=""> </span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="product.php">Boutique</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="connexion.php">Identifiez-vous</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="inscription.php">Inscription</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="profil.php">Mon compte</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  data-toggle="dropdown"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="true">
-                  <span class="nav-label">BackOffice</span><span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="admin/gestion_boutique.php">Boutique</a></li>
-                  <li><a href="admin/gestion_commande.php">Commandes</a></li>
-                  <li><a href="admin/gestion_user.php">Utilisateurs</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <svg
-                    version="1.1"
-                    id="Capa_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 456.029 456.029"
-                    style="enable-background: new 0 0 456.029 456.029"
-                    xml:space="preserve">
-                    <g>
-                      <g>
-                        <path
-                          d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248
-                                          c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
-                      </g>
-                    </g>
-                    <g>
-                      <g>
-                        <path
-                          d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48
-                                          C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064
-                                          c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4
-                                          C457.728,97.71,450.56,86.958,439.296,84.91z" />
-                      </g>
-                    </g>
-                    <g>
-                      <g>
-                        <path
-                          d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296
-                                          c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
-                      </g>
-                    </g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                    <g></g>
-                  </svg>
-                </a>
-              </li>
-              <form class="form-inline">
-                <button class="btn my-2 my-sm-0 nav_search-btn" type="submit">
-                  <i class="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </form>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
-    <!-- end header section -->
-  </div>
+// 1. Contrôler que l'on réceptionne bien toutes les données saisies dans le formulaire en PHP.
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+if(isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] === 'POST'){
+
+
+//  2. Contrôler la validité de l'email (select + rowCount).
+
+// On sélectionne tout dans la BDD à condition que la colonne email dans la BDD soit égale à l'email saisi dans le formulaire
+//                                                              karolynn..gmail.com
+$data = $connect_db->prepare("SELECT * FROM user WHERE email = :email");
+$data->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+$data->execute();
+
+// echo $data->rowCount();
+// Si la condition IF retourne TRUE, l'email est existant en BDD, on entre dans le IF.
+if($data->rowCount()){
+  $errorEmail = '<small class="text-color-danger">Un compte est déjà existant à cette adresse email.</small>';
+}elseif(empty($_POST['email'])){
+  $errorEmail = '<small class="text-color-danger">Merci de saisir une adresse email.</small>';
+}elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+  $errorEmail = '<small class="text-color-danger">Merci de saisir une adresse email valide. (ex: exemple@gmail.com)</small>';
+
+}
+
+$password_regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/"; 
+echo preg_match($password_regex, 'secret') . '<br>'; // returns 0
+echo preg_match($password_regex, '-Secr3t.'); // returns 1
+
+
+// 5. Afficher un message si le champs mot de passe est vide.
+if(empty($_POST['password'])){
+  
+  $errorPassword = '<small class="text-color-danger">Merci de saisir un mot de passe.</small>';
+}elseif (!preg_match($password_regex, $_POST['password'])) {
+  $errorPassword = '<small class="text-color-danger">8 caractères minimum, une majuscule, une minuscule, un chiffre, un caractère spécial ("?!@$%^¨&*-).</small>';  
+}elseif ($_POST['password'] !== $_POST['repeat_password']){
+  $errorPassword = '<small class="text-color-danger">Les mots de passe ne correspondent pas</small>';
+}
+
+}
+
+
+
+
+
+// 3. Afficher un message d'erreur si le champs email est vide.
+//   if(empty($_POST['email'])){
+//    // echo "Le champs email est vide";
+
+//    $errorEmail = 'Erreur';
+//    $error = true;
+//   } 
+ 
+
+// //  4. Contrôler la validité de l'email(filter_var).
+// if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+//   // echo "non valide";
+//    $errorEmail = 'erreur';
+//    $error = true;
+// }
+
+
+
+//   6. Contrôler que les mots de passe correspondent.
+// if(!=($_POST[]))
+//   echo 
+
+
+require_once('include/header.php');
+?>
+
   <!-- inner page section -->
   <section class="inner_page_head">
     <div class="container_fuild">
@@ -164,49 +95,57 @@
       <div class="row">
         <div class="col-lg-8 offset-lg-2">
           <div class="full">
-            <form action="">
+            <form method="post" action="inscription.php">
               <fieldset>
                 <input
                   type="text"
                   placeholder="Enter votre prénom"
                   name="firstName"
-                  required />
+                   />
                 <input
                   type="text"
                   placeholder="Enter votre nom"
                   name="lastName"
-                  required />
+                   />
+                  <?php if(isset($errorEmail)) echo $errorEmail; ?>
                 <input
                   type="text"
                   placeholder="Entrez votre adresse e-mail"
                   name="email"
-                  required />
-                <input
+                 
+                 
+                   class="<?php if(isset($errorEmail)) echo 'border-danger'; ?>"
+                   value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>"
+                   />
+                <input 
                   type="text"
                   placeholder="Entrer votre adresse"
                   name="address"
-                  required />
+                   />
                 <input
                   type="text"
                   placeholder="Entrer votre ville"
                   name="city"
-                  required />
+                   />
                 <input
                   type="text"
                   placeholder="Entrer votre code postal"
                   name="zipcode"
-                  required />
+                   />
+                   <?php if(isset($errorPassword)) echo $errorPassword; ?>
                 <input
-                  type="password"
+                  type="text"
                   placeholder="Enter votre mot de passe"
-                  name="subject"
-                  required />
-                <input
-                  type="repeat_password"
+                  name="password"
+                  class="<?php if(isset($errorPassword)) echo 'border-danger'; ?>"
+                   />
+                  
+                <input                
+                  type="text"
                   placeholder="Répétez votre mot de passe"
-                  name="subject"
-                  required />
-                <input type="submit" value="Submit" />
+                  name="repeat_password"
+                   />
+                <input type="submit" value="Submit" name="submit" />
               </fieldset>
             </form>
           </div>
@@ -217,80 +156,7 @@
   <!-- end why section -->
   <!-- arrival section -->
   <!-- end arrival section -->
-  <!-- footer section -->
-  <footer class="footer_section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 footer-col">
-          <div class="footer_contact">
-            <h4>Nous trouver..</h4>
-            <div class="contact_link_box">
-              <a href="">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span> Coordonnées </span>
-              </a>
-              <a href="">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-                <span> 02 23 45 78 89 </span>
-              </a>
-              <a href="">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <span> demo@gmail.com </span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 footer-col">
-          <div class="footer_detail">
-            <a href="index.php" class="footer-logo"> Famms </a>
-            <p>
-              Nécessaire, ce qui en fait le premier véritable générateur sur Internet.
-              Il utilise un dictionnaire de plus de 200 mots latins, combiné avec
-            </p>
-            <div class="footer_social">
-              <a href="">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-linkedin" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-pinterest" aria-hidden="true"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 footer-col">
-          <div class="map_container">
-            <div class="map">
-              <div id="googleMap"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- footer end -->
-  <div class="cpy_">
-    <p>
-      © 2025 Tous droits réservés par Grégory LACROIX
-    </p>
-  </div>
-  <!-- footer section -->
-  <!-- jQery -->
-  <script src="assets/js-famma/jquery-3.4.1.min.js"></script>
-  <!-- popper js -->
-  <script src="assets/js-famma/popper.min.js"></script>
-  <!-- bootstrap js -->
-  <script src="assets/js-famma/bootstrap.js"></script>
-  <!-- custom js -->
-  <!-- <script src="assets/js-famma/custom.js"></script> -->
-</body>
-
-</html>
+ 
+<?php
+require_once('include/footer.php');
+?>
